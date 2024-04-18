63d0cdd8d460afb6bbdf6db5d339a75b6488e080
Erika Kishino <141463464+erikaak@users.noreply.github.com>
2024-04-18 19:40:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// WebGL shader code for visual effects
+// Shader code with color scheme support
 let vertShader = `
 precision mediump float;
 attribute vec3 aPosition;
@@ -24,64 +24,59 @@ void main() {
     vec2 uv = particles[i].xy;
     float distance = length(st - uv);
     float glow = smoothstep(0.05, 0.0, 1.0 / distance);
-    color += mix(colors[i], vec3(1.0), glow);
+    color += mix(colors[i], vec3(1.0), glow); // Blend particle color with white based on distance
   }
   for (int i = 0; i < int(trailCount); i++) {
     vec2 uv = trail[i];
     float distance = length(st - uv);
-    color += vec3(1.0) / (distance * 100.0);
+    color += vec3(1.0) / (distance * 100.0); // Add a glowing effect for trails
   }
   gl_FragColor = vec4(color, 1.0);
 }
 `;
 
-// Color scheme for particles and other visual elements
-let colorScheme = [
-  [255, 200, 100], [255, 150, 50], [255, 200, 50],
-  [200, 255, 50], [50, 255, 100], [50, 200, 255],
-  [100, 150, 255], [200, 100, 255], [255, 100, 200],
-  [255, 100, 100], [255, 255, 255]
-];
 
+// Particle class
 class Particle {
-  constructor(x, y, vx, vy, color = random(colorScheme)) {
+  constructor(x, y, vx, vy) {
     this.pos = createVector(x, y);
     this.vel = createVector(vx, vy);
-    this.color = color;
+    this.color = random(colorScheme);
   }
-
+  
   move() {
     this.pos.add(this.vel);
   }
-
-  serialize() {
-    return {
-      x: this.pos.x,
-      y: this.pos.y,
-      vx: this.vel.x,
-      vy: this.vel.y,
-      color: this.color
-    };
-  }
-
+  
   explode() {
-    let numParticles = int(random(5, 15));
+    // Create a certain number of smaller particles
+    let numParticles = int(random(5, 15)); // You can adjust the number of particles
     for (let i = 0; i < numParticles; i++) {
-      let angle = random(TWO_PI);
-      let speed = random(1, 3);
-      let vx = speed * cos(angle);
-      let vy = speed * sin(angle);
-      let newParticle = new Particle(this.pos.x, this.pos.y, vx, vy, this.color);
+      // Create particles with random velocities around the original particle's position
+      let newParticle = new Particle(this.pos.x, this.pos.y, random(-5, 5), random(-5, 5));
+      // Add the new particle to the global particles array
       particles.push(newParticle);
-      // Send new particle data to Firebase
-      database.ref('particles').push(newParticle.serialize());
     }
   }
 }
 
+// Array to hold particles
 let particles = [];
 let graphics; // 2D graphics buffer for text
 let objects = []; // Array to hold 3D objects
+let colorScheme = [
+  [255, 200, 100],  // Bright orange
+  [255, 150, 50],   // Bright orange-yellow
+  [255, 200, 50],   // Bright yellow
+  [200, 255, 50],   // Bright lime green
+  [50, 255, 100],   // Bright green
+  [50, 200, 255],   // Bright cyan
+  [100, 150, 255],  // Bright light blue
+  [200, 100, 255],  // Bright violet
+  [255, 100, 200],  // Bright magenta
+  [255, 100, 100],  // Bright pink
+  [255, 255, 255],  // White
+];
 
 function setup() {
   createCanvas(windowWidth * 2/3, windowHeight, WEBGL).parent('webglCanvas');
@@ -90,100 +85,113 @@ function setup() {
   graphics.textAlign(CENTER, CENTER);
   pixelDensity(1);
   noCursor();
+}
 
-  // Listen for updates from Firebase
-  listenForUpdates();
-  listenForParticleUpdates();
-
-  // Setup button to reset view
-  const resetViewBtn = createButton('Reset View');
-  resetViewBtn.mousePressed(resetView);
+function updateText() {
+  let inputText = document.getElementById('userInput').value.trim();
+  let selectedFont = document.getElementById('fontSelector').value;
+  let selectedColor = document.getElementById('colorSelector').value;
+  if (inputText !== "") {
+      objects.push({
+          x: random(-200, 200),
+          y: random(-200, 200),
+          z: random(-200, 200),
+          speed: random(1, 5),
+          direction: random([-1, 1]),
+          color: selectedColor,
+          font: selectedFont,
+          text: inputText
+      });
+      document.getElementById('userInput').value = '';
+  }
 }
 
 function draw() {
   background(0);
   orbitControl();
-  displayObjects();
-  displayParticles();
-}
-
-function displayObjects() {
   objects.forEach(obj => {
-    push();
-    translate(obj.x, obj.y, obj.z);
-    fill(obj.color);
-    textFont(obj.font);
-    textSize(24);
-    text(obj.text, 0, 0);
-    pop();
+      push();
+      translate(obj.x, obj.y, obj.z);
+      fill(obj.color);
+      textFont(obj.font);
+      textSize(24); // Adjust text size if necessary
+      text(obj.text, 0, 0); // Draw text at object's location
+      pop();
 
-    obj.z += obj.speed * obj.direction;
-    if ((obj.direction === 1 && obj.z > 200) || (obj.direction === -1 && obj.z < -200)) {
-      obj.direction *= -1;
-    }
+      // Update position and direction
+      obj.z += obj.speed * obj.direction;
+      if ((obj.direction === 1 && obj.z > 200) || (obj.direction === -1 && obj.z < -200)) {
+          obj.direction *= -1; // Change direction upon reaching a certain point
+      }
+
   });
-}
 
-function displayParticles() {
+  graphics.clear();
+  objects.forEach(obj => {
+      graphics.fill(obj.color);
+      graphics.textFont(obj.font);
+      graphics.text(obj.text, obj.x + width / 2, obj.y + height / 2, obj.z);
+  
+      push();
+      translate(obj.x, obj.y, obj.z);
+      fill(obj.color);
+      box(20); // Drawing a simple box
+      pop();
+    });
+  image(graphics, -width / 2, -height / 2);
+
+  // Display particles
   particles.forEach(p => {
-    fill(p.color);
-    ellipse(p.pos.x, p.pos.y, 8, 8);
-    p.move();
+      fill(p.color);
+      ellipse(p.pos.x, p.pos.y, 8, 8);
+      p.move();
   });
 }
 
 function mouseDragged() {
-  let newParticle = new Particle(pmouseX - width / 2, pmouseY - height / 2, mouseX - pmouseX, mouseY - pmouseY);
-  particles.push(newParticle);
-  database.ref('particles').push(newParticle.serialize());
+  particles.push(new Particle(pmouseX - width / 2, pmouseY - height / 2, mouseX - pmouseX, mouseY - pmouseY));
 }
 
-function listenForParticleUpdates() {
-  const particleRef = firebase.database().ref('particles');
-  particleRef.on('child_added', snapshot => {
-    const p = snapshot.val();
-    particles.push(new Particle(p.x, p.y, p.vx, p.vy, p.color));
-  });
+function mousePressed() {
+  // Check if the mouse button pressed is the left mouse button
+  if (mouseButton === LEFT) {
+    // Randomly choose between changing color and exploding
+    let randomAction = random();
+    if (randomAction < 0.5) {
+      // Change color
+      let particleIndex = int(random(particles.length)); // Choose a random particle
+      particles[particleIndex].color = random(colorScheme); // Change its color to a random color
+    } else {
+      // Explode
+      let particleIndex = int(random(particles.length)); // Choose a random particle
+      particles[particleIndex].explode(); // Make it explode
+      particles.splice(particleIndex, 1); // Remove the original particle
+    }
+  }
+  
+  // Clear particles if right-clicked
+  if (mouseButton === RIGHT) {
+    particles = [];
+  }
 }
 
-function listenForUpdates() {
+function initFirebaseObjects() {
   const database = firebase.database();
   database.ref('userInputs').on('child_added', function(snapshot) {
-    const data = snapshot.val();
-    if (data) {
-      objects.push({
-        x: random(-200, 200),
-        y: random(-200, 200),
-        z: random(-200, 200),
-        speed: random(1, 5),
-        direction: random([-1, 1]),
-        color: data.color,
-        font: data.font,
-        text: data.text
-      });
-    }
+      const data = snapshot.val();
+      addNewObject(data);
   });
 }
 
-function resetView() {
-    if (objects.length > 0) {
-        // Calculate the centroid of text objects only
-        let sumX = 0, sumY = 0, sumZ = 0;
-
-        for (let obj of objects) {
-            sumX += obj.x;
-            sumY += obj.y;
-            sumZ += obj.z;
-        }
-
-        let centerX = sumX / objects.length;
-        let centerY = sumY / objects.length;
-        let centerZ = sumZ / objects.length - 500; // Subtracted 500 to set a reasonable default distance
-
-        // Set the camera to look at the centroid of text objects
-        camera(centerX, centerY, centerZ + (height / 2) / tan(PI * 30.0 / 180.0), centerX, centerY, centerZ, 0, 1, 0);
-    } else {
-        // Reset to default view if there are no text objects
-        camera(0, 0, (height/2) / tan(PI/6), 0, 0, 0, 0, 1, 0);
-    }
+function addNewObject(data) {
+  objects.push({
+      x: random(-200, 200),
+      y: random(-200, 200),
+      z: random(-200, 200),
+      speed: random(1, 5),
+      direction: random([-1, 1]),
+      color: data.color,
+      font: data.font,
+      text: data.text
+  });
 }
