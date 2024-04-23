d442ee1655ea6229c638a013b9414ab491d1a1ed
Erika Kishino <141463464+erikaak@users.noreply.github.com>
2024-04-23 11:31:14 -0400

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,15 +1,1 @@
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCnET0MG8IWwTtvX1Y5m9IKRetZRHUJLY8",
-    authDomain: "dsgn0010-405.firebaseapp.com",
-    databaseURL: "https://dsgn0010-405-default-rtdb.firebaseio.com",
-    projectId: "dsgn0010-405",
-    storageBucket: "dsgn0010-405.appspot.com",
-    messagingSenderId: "672215177148",
-    appId: "1:672215177148:web:9b4a888eaa64f67825fdb9"
 
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
