28ee2df65f7026b3ed16bf1da5d62411096d6d61
Erika Kishino <141463464+erikaak@users.noreply.github.com>
2024-04-29 14:02:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ void main() {
   for (int i = 0; i < int(particleCount); i++) {
     vec2 uv = particles[i].xy;
     float distance = length(st - uv);
-    float glow = smoothstep(0.05, 0.0, distance);
+    float glow = smoothstep(0.05, 0.0, 1.0 / distance);
     color += mix(colors[i], vec3(1.0), glow);
   }
   for (int i = 0; i < int(trailCount); i++) {
@@ -35,174 +35,76 @@ void main() {
 }
 `;
 
-// Color scheme for particles and other visual elements
-let colorScheme = [
-  [255, 200, 100], [255, 150, 50], [255, 200, 50],
-  [200, 255, 50], [50, 255, 100], [50, 200, 255],
-  [100, 150, 255], [200, 100, 255], [255, 100, 200],
-  [255, 100, 100], [255, 255, 255]
-];
-
-class Particle {
-  constructor(x, y, vx, vy, color = random(colorScheme)) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(vx, vy);
-    this.color = color;
-  }
-
-  move() {
-    this.pos.add(this.vel);
-  }
-
-  serialize() {
-    return {
-      x: this.pos.x,
-      y: this.pos.y,
-      vx: this.vel.x,
-      vy: this.vel.y,
-      color: this.color
-    };
-  }
-
-  explode() {
-    let numParticles = int(random(5, 15));
-    for (let i = 0; i < numParticles; i++) {
-      let angle = random(TWO_PI);
-      let speed = random(1, 3);
-      let vx = speed * cos(angle);
-      let vy = speed * sin(angle);
-      let newParticle = new Particle(this.pos.x, this.pos.y, vx, vy, this.color);
-      particles.push(newParticle);
-      // Send new particle data to Firebase
-      database.ref('particles').push(newParticle.serialize());
-    }
-  }
-}
-
-class Object3D {
-    constructor(x, y, z, text, font, color) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.text = text;
-        this.font = font;
-        this.color = color;
-        this.size = 24; // default size
-        this.isSelected = false;
-        this.scaleFactor = 1.5; // how much the text scales up when clicked
-    }
-
-    display() {
-        push();
-        translate(this.x, this.y, this.z);
-        fill(this.color);
-        textFont(this.font);
-        textSize(this.size * (this.isSelected ? this.scaleFactor : 1));
-        text(this.text, 0, 0);
-        pop();
-    }
-
-    checkIfClicked(px, py) {
-        // Simple distance check to see if the click is within bounds
-        let d = dist(px, py, this.x, this.y);
-        if (d < 50) { // assuming the clickable area is within 50 pixels radius
-            this.isSelected = !this.isSelected; // toggle selection
-        }
-    }
-}
-
-let particles = [];
-let graphics; // 2D graphics buffer for text
-let objects = []; // Array to hold 3D objects
+// Array to hold 3D text objects
+let objects = [];
 
 function setup() {
   createCanvas(windowWidth * 2/3, windowHeight, WEBGL).parent('webglCanvas');
-  graphics = createGraphics(windowWidth / 3, windowHeight);
-  graphics.textSize(20);
-  graphics.textAlign(CENTER, CENTER);
   pixelDensity(1);
   noCursor();
-
-  // Listen for updates from Firebase
-  listenForUpdates();
-  listenForParticleUpdates();
-
-  // Setup button to reset view
-  const resetViewBtn = select('#resetViewBtn');
-  resetViewBtn.mousePressed(resetView);
+  // Setup Firebase
+  const firebaseConfig = {
+    // Your Firebase config
+  };
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  listenForUpdates(database);
 }
 
 function draw() {
   background(0);
   orbitControl();
+
   objects.forEach(obj => {
-    obj.display();
+    push();
+    let screenPos = screenPosition(obj.x, obj.y, obj.z);
+    let d = dist(mouseX, mouseY, screenPos.x, screenPos.y);
+    
+    // Hover effect
+    let textSizeVal = 24; // Default text size
+    let boxSize = 20; // Default box size
+    if (d < 50) { // Threshold for hover effect
+      textSizeVal = 30; // Increased text size
+      boxSize = 30; // Increased box size
+    }
+
+    // Update position
+    obj.x += obj.speed * obj.direction;
+    if ((obj.direction === 1 && obj.x > 200) || (obj.direction === -1 && obj.x < -200)) {
+      obj.direction *= -1;
+    }
+
+    // Draw text
+    translate(obj.x, obj.y, obj.z);
+    fill(obj.color);
+    textFont(obj.font);
+    textSize(textSizeVal);
+    text(obj.text, 0, 0);
+    pop();
+
+    // Draw box
+    push();
+    translate(obj.x, obj.y, obj.z);
+    fill(obj.color);
+    box(boxSize);
+    pop();
   });
-  displayParticles();
 }
 
-function displayParticles() {
-  particles.forEach(p => {
-    fill(p.color);
-    ellipse(p.pos.x, p.pos.y, 8, 8);
-    p.move();
-  });
-}
-
-function mousePressed() {
-  let mx = mouseX - width / 2; // Adjusting mouse x for WEBGL coordinates
-  let my = mouseY - height / 2; // Adjusting mouse y for WEBGL coordinates
-  objects.forEach(obj => {
-      obj.checkIfClicked(mx, my);
-  });
-}
-
-function listenForParticleUpdates() {
-  const particleRef = firebase.database().ref('particles');
-  particleRef.on('child_added', snapshot => {
-    const p = snapshot.val();
-    particles.push(new Particle(p.x, p.y, p.vx, p.vy, p.color));
-  });
-}
-
-function listenForUpdates() {
-  const database = firebase.database();
+function listenForUpdates(database) {
   database.ref('userInput').on('child_added', function(snapshot) {
     const data = snapshot.val();
     if (data) {
-      let newObj = new Object3D(
-          random(-200, 200), random(-200, 200), random(-200, 200),
-          data.text, data.font, data.color
-      );
-      objects.push(newObj);
+      objects.push({
+        x: random(-200, 200),
+        y: random(-200, 200),
+        z: random(-200, 200),
+        speed: random(1, 5),
+        direction: random([-1, 1]),
+        color: data.color,
+        font: data.font,
+        text: data.text
+      });
     }
   });
 }
-
-function resetView() {
-  let centerX = 0, centerY = 0, centerZ = 0;
-  let textCount = 0;
-  objects.forEach(obj => {
-      if (typeof obj.text !== 'undefined') {
-          centerX += obj.x;
-          centerY += obj.y;
-          centerZ += obj.z;
-          textCount++;
-      }
-  });
-
-  if (textCount > 0) {
-      centerX /= textCount;
-      centerY /= textCount;
-      centerZ /= textCount;
-      camera(centerX, centerY, centerZ + 500, centerX, centerY, centerZ, 0, 1, 0);
-  } else {
-      camera();
-  }
-
-  if (particles.length === 0 && objects.length === 0) {
-      database.ref('particles').remove();
-      database.ref('userInput').remove();
-  }
-}
-
